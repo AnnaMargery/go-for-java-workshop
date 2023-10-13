83ba18c03c35f8470d15d7f2dc58bbbaf306b5ae
Anna Margery <anna.szewc@gmail.com>
2023-10-13 23:12:55 +0200

    WorkshopEmployeeStatsService Added implementation for 2 methods (1 left))

--- src/main/java/org/goforjava/domain/WorkshopEmployeeStatsService.java
+++ src/main/java/org/goforjava/domain/WorkshopEmployeeStatsService.java
@@ -18,63 +18,92 @@ public class WorkshopEmployeeStatsService implements EmployeeStatsService {
 
     @Override
     public List<Employee> findEmployeesOlderThen(long years) {
-        return employeeDB.findAll().stream()
-                .filter(employee -> LocalDate.now().getYear() - employee.getBirthDate().getYear() >= years)
+        return employeeDB
+                .findAll()
+                .stream()
+                .filter(employee -> LocalDate.now()
+                        .getYear() - employee
+                        .getBirthDate()
+                        .getYear() >= years)
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<Employee> findThreeTopCompensatedEmployees() {
-        return employeeDB.findAll().stream()
-                .sorted(Comparator.comparingLong(Employee::getGrossSalary).reversed())
-                .limit(3).collect(Collectors.toList());
+        return employeeDB
+                .findAll()
+                .stream()
+                .sorted(Comparator
+                        .comparingLong(Employee::getGrossSalary)
+                        .reversed())
+                .limit(3)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<Department> findDepartmentWithLowestCompensationAverage() {
-        // szukamy sredniej wynagroszen w depatamencie
 
 
 
         return Optional.empty();
     }
 
+//    @Override
+//    public List<Employee> findEmployeesBasedIn(Localtion localtion) {
+//
+//        List<Employee> employeesByLocation = new ArrayList<>();
+//        List<Department> departmentByLocation = new ArrayList<>();
+//
+//        for (Department department : departmentDB.findAll()) {
+//            if (department.getLocation() == localtion) {
+//                departmentByLocation.add(department);
+//            }
+//        }
+//
+//        for (Department department : departmentByLocation) {
+//            for (Employee employee : employeeDB.findAll()) {
+//                if (department.getId().equals(employee.getDepartmentId())) {
+//                    employeesByLocation.add(employee);
+//                }
+//            }
+//        }
+//        return employeesByLocation;
+//    }
+
     @Override
     public List<Employee> findEmployeesBasedIn(Localtion localtion) {
-
-        List<Employee> employeesByLocation = new ArrayList<>();
-        List<Department> departmentByLocation = new ArrayList<>();
-
-        for (Department department : departmentDB.findAll()) {
-            if (department.getLocation() == localtion) {
-                departmentByLocation.add(department);
-            }
-        }
-
-        for (Department department : departmentByLocation) {
-            for (Employee employee : employeeDB.findAll()) {
-                if (department.getId().equals(employee.getDepartmentId())) {
-                    employeesByLocation.add(employee);
-                }
-            }
-        }
-        return employeesByLocation;
+       return employeeDB
+                .findAll()
+                .stream()
+                .filter(e -> departmentDB
+                        .findById(e.getDepartmentId())
+                        .get()
+                        .getLocation()
+                        .equals(localtion))
+                .collect(Collectors.toList());
     }
-
-//todo zastanowic sie nad implementacja ze stremami.
 
     @Override
     public Map<Integer, Long> countEmployeesByHireYear() {
-        return employeeDB.findAll().stream()
-                .collect(Collectors.groupingBy(employee -> employee.getHireDate().getYear(), Collectors.counting()));
+        return employeeDB
+                .findAll()
+                .stream()
+                .collect(Collectors
+                        .groupingBy(employee -> employee
+                                .getHireDate()
+                                .getYear(), Collectors
+                                .counting()));
     }
-
 
     @Override
     public Map<Localtion, Long> countEmployeesByLocation() {
-
-
-
-        return Map.of();
+        return employeeDB
+                .findAll()
+                .stream()
+                .collect(Collectors
+                        .groupingBy(employee -> departmentDB
+                                .findById(employee.getDepartmentId())
+                        .get()
+                        .getLocation(),Collectors.counting()));
     }
 }
